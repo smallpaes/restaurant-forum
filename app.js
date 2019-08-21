5a4d80e3ac4243323c1015c89f31690060024e8e
Mike <mike.huang.mikank@gmail.com>
2019-08-21 16:14:50 +0800

    Update port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // set up handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
