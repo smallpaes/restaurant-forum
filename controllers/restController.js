b843c82ea092f3b1d909249fa5d1a9849321b92b
Mike <mike.huang.mikank@gmail.com>
2019-08-26 19:45:05 +0800

    Add show all restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,5 +1,21 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 module.exports = {
-  getRestaurants: (req, res) => {
+  getRestaurants: async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll({ include: Category })
+      const data = restaurants.map(restaurant => ({
+        ...restaurant.dataValues,
+        description: restaurant.dataValues.description.substring(0, 50)
+      }))
+      return res.render('restaurants', { restaurants: data })
+    } catch (err) {
+      console.log(err)
+    }
+
+
     return res.render('restaurants')
   }
 }
