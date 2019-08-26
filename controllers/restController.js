283215f70f981ea2f892b508b16d364a82b67c54
Mike <mike.huang.mikank@gmail.com>
2019-08-26 20:25:33 +0800

    Add category filter and category nav

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,13 +4,26 @@ const Category = db.Category
 
 module.exports = {
   getRestaurants: async (req, res) => {
+    const whereQuery = {}
+    let categoryId = ''
+    // check if any category is selected
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+
     try {
-      const restaurants = await Restaurant.findAll({ include: Category })
+      const restaurants = await Restaurant.findAll({ include: Category, where: whereQuery })
+      const categories = await Category.findAll()
       const data = restaurants.map(restaurant => ({
         ...restaurant.dataValues,
         description: restaurant.dataValues.description.substring(0, 50)
       }))
-      return res.render('restaurants', { restaurants: data })
+      return res.render('restaurants', {
+        restaurants: data,
+        categories,
+        categoryId
+      })
     } catch (err) {
       console.log(err)
     }
