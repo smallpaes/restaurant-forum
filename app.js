03c12d3255a60f39dff7b356401b99b280ea5bd0
Mike <mike.huang.mikank@gmail.com>
2019-08-28 16:53:29 +0800

    Alter sync setting

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
   // 跟資料庫同步
-  db.sequelize.sync()
+  db.sequelize.sync({ force: true })
   console.log(`App is listening on port ${port}`)
 })
 
