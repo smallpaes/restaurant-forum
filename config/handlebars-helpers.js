a792d363da08438d3d920058d9992b4550f05ac6
Mike <mike.huang.mikank@gmail.com>
2019-09-01 13:46:51 +0800

    Move handlebar helpers to the same config file

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -7,5 +7,18 @@ module.exports = {
   },
   moment: function (time) {
     return moment(time).fromNow()
+  },
+  isCurrentPage: function (currentPage, pageNumber, options) {
+    if (currentPage !== pageNumber) return
+    return 'active'
+  },
+  getSort: function (sortItem, options) {
+    if (!sortItem) return
+    const sortBy = Object.keys(sortItem)[0]
+    return `&sortBy=${sortBy}:${sortItem[sortBy]}`
+  },
+  changeOrder: function (order, options) {
+    if (!order) return 'DESC'
+    return order === 'DESC' ? 'ASC' : 'DESC'
   }
 }
